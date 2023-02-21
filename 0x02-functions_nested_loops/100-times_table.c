0136d953af280b852a80a8d0c6237d08b77286a7
yeabgenet <yeabsiragenet48@gmail.com>
2023-02-22 01:25:48 +0300

    v

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-} 
+}
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -32,7 +32,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} 
+}
 else if (k >= 100)
 {
 _putchar(',');
