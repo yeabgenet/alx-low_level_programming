cdddf6799cf186d89436b86990aa3a4aaff4d31f
yeabgenet <yeabsiragenet48@gmail.com>
2023-02-21 23:06:10 +0300

    Check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,9 @@
 #include "main.h"
 /**
-		
- * main - check the code.
-		
- *
-		
- * Return: Always 0.
-		
- */
+* main - check the code.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 int r;
@@ -19,6 +15,5 @@ r = _islower(108);
 _putchar(r + '0');
 _putchar('\n');
 return (0);
-		
 }
 
